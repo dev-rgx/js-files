9b2f0123772aeec671266d5ceb1810b8921b501f
Gaurav Raj <rjgaurav05@gmail.com>
2025-05-26 19:40:42 +0530

    Updated basic.js

--- javaScript/basic.js
+++ javaScript/basic.js
@@ -2,5 +2,6 @@ console.log("Gaurav")
 const accountId = 19203
 let emailId = "rjgaurav05@gmail.com"
 var password = "12345"
-console.log(accountId)
-console.log(emailId)
+// console.log(accountId)
+// console.log(emailId)
+console.table([accountId, emailId, password])
